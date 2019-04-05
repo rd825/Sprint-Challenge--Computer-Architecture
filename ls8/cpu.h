094534c45696f50d439956f7a1254cdca075ae1e
Rahul Desai <rd825@georgetown.edu>
2019-04-05 12:04:31 -0400

    defined stretch ALU ops in cpu.h

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -32,7 +32,16 @@ enum alu_op
   // Add more here
   ALU_MUL,
   ALU_ADD,
-  ALU_CMP
+  ALU_CMP,
+  // STRETCH
+  ALU_AND,
+  ALU_OR,
+  ALU_XOR,
+  ALU_NOT,
+  ALU_SHL,
+  ALU_SHR,
+  ALU_MOD,
+
 };
 
 // Instructions
@@ -46,9 +55,16 @@ enum alu_op
 // TODO: more instructions here. These can be used in cpu_run().
 
 // ALU
-#define MUL 0b10100010 // ALU MULTIPLY
-#define ADD 0b10100000 // ALU ADD
-#define CMP 0b10100111 // ALU COMPARE
+#define MUL 0b10100010 // MULTIPLY
+#define ADD 0b10100000 // ADD
+#define CMP 0b10100111 // COMPARE
+#define AND 0b10101000 // AND
+#define OR 0b10101010  // OR
+#define XOR 0b10101011 // EXCLUSIVE OR
+#define NOT 0b01101001 // NOT
+#define SHL 0b10101100 // SHIFT LEFT
+#define SHR 0b10101101 // SHIFT RIGHT
+#define MOD 0b10100100 // MODULO
 
 // STACK
 #define PUSH 0b01000101 // STACK PUSH
